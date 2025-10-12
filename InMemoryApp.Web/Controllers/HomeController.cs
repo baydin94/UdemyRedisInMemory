938db17a03724ab185e086c7f13e2edd64dfd3f1
Baransel Aydın <72010241+baydin94@users.noreply.github.com>
2025-10-12 22:17:16 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using InMemoryApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _memoryCache;
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            _memoryCache.Set("name","Baransel", TimeSpan.FromMinutes(1));
+            var name = _memoryCache.Get<string>("name");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
